187d6cb14d89a3938cdc6cf6817d54db3beb4bd7
acol248 <alex.collyer@acol.dev>
2025-12-24 23:41:04 +0000

    Experiment with requiring user iteraction to exit

--- getting-started/hello.cpp
+++ getting-started/hello.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
   std::cout << "Hello, World!" << std::endl;
+
+  std::cout << "Press Enter to exit...\n";
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   return 0;
 }
